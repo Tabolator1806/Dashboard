2a80bb2c29c851997c33cce4cb14462beefe82f2
Tab <tabolator1@gmail.com>
2026-02-23 13:40:58 +0100

    kierunek wiatru

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -9,6 +9,7 @@
     <div id="info">
       Temperature: {{weather.temperatura}} C
       Pressure: {{weather.cisnienie}} hPa
+      Wind: {{windDirection}} {{weather.predkosc_wiatru}} km/h
     </div>
   </div>
 </template>
@@ -22,8 +23,38 @@ export default {
   },
   created(){
     getWeather('krakow').then(data=>{
+      console.log(data)
       this.weather = data
     })
+  },
+  computed: {
+    windDirection(){
+      const direction = this.weather.kierunek_wiatru
+      if (direction>330&&direction<30){
+        return "E"
+      }
+      else if (direction>30&&direction<60){
+        return "NE"
+      }
+      else if (direction>60&&direction<120){
+        return "N"
+      }
+      else if (direction>120&&direction<160){
+        return "NW"
+      }
+      else if (direction>160&&direction<210){
+        return "W"
+      }
+      else if (direction>210&&direction<240){
+        return "SW"
+      }
+      else if (direction>240&&direction<300){
+        return "S"
+      }
+      else if (direction>300&&direction<330){
+        return "SE"
+      }
+    }
   },
 }
 </script>
