693f4e600f9ab004a628c198eb6422c870486225
Tab <tabolator1@gmail.com>
2026-03-19 23:55:42 +0100

    steam nie działa :c

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,16 @@
 
 async function get(url){
-  const response = await fetch(url)
+  const response = await fetch(url,{
+    method:"get",
+  })
   var message = await response.text()
   message = JSON.parse(message)
   return message
 }
 const getWeather = (city) => get(`https://danepubliczne.imgw.pl/api/data/synop/station/${city}`)
+const getSteam = (id) => get(`https://api.steamplaytime.com/profile/${id}`)
 
 export {
-  getWeather
+  getWeather,
+  getSteam
 }
--- src/components/Steam.vue
+++ src/components/Steam.vue
@@ -3,3 +3,20 @@
 		<div id="title">Steam Stats</div>
 	</div>
 </template>
+<script>
+	import { getSteam } from '@/api'
+	export default{
+		data() {
+			return {
+				"data":{},
+			}
+		},
+		created(){
+			getSteam('76561198391377502').then(data=>{
+				console.log(data)
+				this.data = data
+			})
+		},
+	}
+</script>
+
