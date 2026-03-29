e57f014e4b6112075edd438ca0c64c3ba87c6c09
Tab <tabolator1@gmail.com>
2026-03-29 01:40:49 +0100

    clock added

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -5,7 +5,9 @@
 			<div id="clock">
 				<img alt="face" src="@/assets/images/clock_face2.png"/>
 				<img alt="hour" :style="{transform: 'rotate('+setHourArm+'turn)'}" src="@/assets/images/clock_hour_arm.png"/>
+				<img alt="minutes" :style="{transform: 'rotate('+setMinutesArm+'turn)'}" src="@/assets/images/clock_hour_arm.png"/>
 			</div>
+			{{hour}}:{{minutes}}:{{seconds}}
 			<div id="info">
 				Todays Date: {{addZero(day)}}.{{addZero(month)}}.{{year}}<br/>
 				<div id="bar"><div id="progress" :style="{width: yearProgression+'%'}">{{yearProgression}}%</div></div>
@@ -23,21 +25,12 @@
 				"day":0,
 				"hour":0,
 				"minutes":0,
-				"yearProgression":0,
+				"seconds":0,
       	"days":[0,31,59,90,120,151,181,212,242,273,304,334],
-				"daysPast":0
 			}
 		},
 		created(){
-			const currentDate = new Date(Date.now())
-			this.year=currentDate.getFullYear()
-			this.month=currentDate.getMonth()+1
-			this.day=currentDate.getDate()
-			this.hour=currentDate.getHours()
-			this.minutes=currentDate.getMinutes()
-			this.daysPast = this.days[this.month] + this.day
-			this.yearProgression = Math.round(((this.daysPast)/365)*10000)/100
-			console.log((this.hour*100 + this.minutes)/1200)
+			this.getTime()
 		},
 		methods:{
 			addZero(inttime){
@@ -47,12 +40,35 @@
 				}
 				return time
 			},
+			getTime(){
+				const currentDate = new Date(Date.now())
+				this.month=currentDate.getMonth()+1
+				this.year=currentDate.getFullYear()
+				this.day=currentDate.getDate()
+				this.hour=currentDate.getHours()
+				this.minutes=currentDate.getMinutes()
+				this.seconds=currentDate.getSeconds()
+				console.log((this.hour*100 + this.minutes)/1200)
+				setTimeout(()=>{
+						this.getTime()
+					},1000
+				)
+			}
 		},
 		computed:{
 			setHourArm(){
 				return (this.hour*100 + this.minutes)/1200
+			},
+			setMinutesArm(){
+				return this.minutes/60
+			},
+			daysPast(){
+				return this.days[this.month] + this.day
+			},
+			yearProgression(){
+				return Math.round(((this.daysPast)/365)*10000)/100
 			}
-		}
+		},
 	}
 </script>
 <style>
@@ -66,6 +82,11 @@
 		height:150px;
 	}
 	img[alt="hour"]{
+		position:absolute;
+		left:0;
+		top:0;
+	}
+	img[alt="minutes"]{
 		position:absolute;
 		left:0;
 		top:0;
