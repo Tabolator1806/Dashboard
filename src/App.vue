f8ba79e58be1e63dedcc321a8c23fce1557fef40
Tab <tabolator1@gmail.com>
2026-04-26 11:40:51 +0200

    Dark and light mode

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,13 @@
     </header>
     <RouterView/>
 </template>
+<script>
+export default {
+  created() {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      document.body.style.background= event.matches? "black" : "white"
+    });
+
+  }
+}
+</script>
