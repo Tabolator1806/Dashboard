23511f6edc9195205845e1054748c01b044e8859
Tab <tabolator1@gmail.com>
2026-04-29 22:40:39 +0200

    nwm

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+// @ts-ignore
 import App from './App.vue'
 import './styles/main.css'
-import router from './router'
+// @ts-ignore
+import router from './router/index.js'
 const app = createApp(App)
 app.use(router).mount('#app')
